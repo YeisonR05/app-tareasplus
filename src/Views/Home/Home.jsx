0ebdf66f326dfa1c06dd0116a7eee0c655eea6cd
YeisonR05 <yeijoh91@gmail.com>
2023-07-06 11:03:07 -0500

    Home Resposive

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -11,29 +11,29 @@ const Home = () => {
   ];
 
   return (
-    <div className="flex">
-      <div className="w-1/2 p-12">
+    <div className="flex flex-col md:flex-row">
+      <div className="w-full md:w-1/2 p-12">
         <div className="flex flex-col items-start justify-center">
           {subscriptionPlans.map((plan, index) => (
             <div
               key={index}
-              className="w-full h-[450px] p-4"
+              className="w-full h-auto md:h-[450px] p-4"
             >
               <div className="flex items-center">
-                <h2 className="text-7xl font-bold text-black text-teal-600 mt-6">
+                <h2 className="text-5xl md:text-7xl font-bold text-black text-teal-600 mt-6">
                   {plan.title}
                 </h2>
               </div>
               <ul className="text-black text-start">
                 {plan.text.map((text, index) => (
-                  <li key={index} className="mt-2 text-2xl text-teal-600">
+                  <li key={index} className="mt-2 text-xl md:text-2xl text-teal-600">
                     {text} 
                   </li>
                 ))}
               </ul>
               <ul className="text-black text-start mt-2">
                 {plan.features.map((feature, index) => (
-                  <li key={index} className="mt-6 text-lg text-justify">
+                  <li key={index} className="mt-6 text-base md:text-lg text-justify">
                     {feature}
                   </li>
                 ))}
@@ -42,13 +42,13 @@ const Home = () => {
           ))}
         </div>
       </div>
-      <div className="w-1/2">
+      <div className="w-full md:w-1/2">
         <div className="p-4">
-          <div className="w-full h-[550px] p-4 mx-auto">
+          <div className="w-full h-[300px] md:h-[550px] p-4 mx-auto">
             <img
               src={FotoHome}
               alt="TAREASPLUS"
-              className="object-contain w-full h-full "
+              className="object-contain w-full h-full"
             />
           </div>
         </div>
@@ -58,5 +58,3 @@ const Home = () => {
 }
 
 export default Home;
-
-
