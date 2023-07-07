6d146d1c0c42603375c7b1c9f744471e6f9b0327
YeisonR05 <yeijoh91@gmail.com>
2023-07-07 10:53:38 -0500

    Responsive NavBar 7

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
 import Logo from "../../Multimedia/Logo.png";
 import ContactButton from "../ContactButton/ContactButton";
+import {BiMenu} from 'react-icons/bi'
 
 function NavItem({ href, text }) {
   return (
@@ -17,10 +18,7 @@ function NavItem({ href, text }) {
 
 function NavItemMobile({ href, text }) {
   return (
-    <NavLink
-      to={href}
-      className="text-black text-xs "
-    >
+    <NavLink to={href} className="text-black text-xm">
       {text}
     </NavLink>
   );
@@ -38,7 +36,18 @@ const NavBar = () => {
     <nav className="bg-[#18E4BE] p-[0.7rem] sticky top-0 z-10">
       <div className="flex justify-between items-center max-w-[1150px] mx-auto">
         <div className="flex items-center gap-[1rem]">
-          <NavLink to="/" className="w-[55px] h-[55px]">
+          {isMobile && (
+            <button
+              className="text-black font-bold hover:text-gray-800"
+              onClick={toggleNav}
+            >
+              <div className="px-4 flex justify-center">
+              <BiMenu size={20} />
+              </div>
+              Menu
+            </button>
+          )}
+          <NavLink to="/" className="w-[55px] h-[55px] ml-auto">
             <img
               src={Logo}
               alt="TAREASPLUS"
@@ -48,17 +57,7 @@ const NavBar = () => {
         </div>
         {isMobile ? (
           // Mobile navigation
-          <>
-            <button
-              className="text-black font-bold hover:text-gray-800"
-              onClick={toggleNav}
-            >
-              Menu
-            </button>
-            {isNavOpen && (
-              <MobileNav />
-            )}
-          </>
+          isNavOpen && <MobileNav />
         ) : (
           // Desktop navigation
           <DesktopNav />
@@ -79,26 +78,42 @@ const DesktopNav = () => (
   </div>
 );
 
-const MobileNav = () => (
-  <div className="text-xxs absolute mt-40 w-35 sm:w-35 bg-gray-100 rounded-lg shadow-lg z-10">
-    <div >
-      <NavItemMobile href="/" text="Inicio" />
-    </div>
-    <div>
-      <NavItemMobile href="/services" text="Servicios" />
-    </div>
-    <div>
-      <NavItemMobile href="/metodology" text="Metodología" />
-    </div>
-    <div>
-      <NavItemMobile href="/priceswarranty" text="Precios y Garantía" />
-    </div>
-    <div>
-      <NavItemMobile href="/about" text="Acerca de Nosotros" />
-    </div>
-  </div>
-);
+const MobileNav = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
+  const toggleSubMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="text-xxs absolute left-0 top-16 w-48 sm:w-56 bg-gray-100 rounded-lg shadow-lg z-10">
+      <div className="mt-1">
+        <NavItemMobile href="/" text="Inicio" />
+      </div>
+      <div className="mt-1">
+        <NavItemMobile href="/services" text="Servicios" />
+      </div>
+      <div className="mt-1">
+        <NavItemMobile href="/metodology" text="Metodología" />
+      </div>
+      <div className={`mt-1 ${isOpen ? 'block' : 'hidden'}`}>
+        <NavItemMobile href="/priceswarranty" text="Precios y Garantía" />
+      </div>
+      <div className={`mt-1 ${isOpen ? 'block' : 'hidden'}`}>
+        <NavItemMobile href="/about" text="Acerca de Nosotros" />
+      </div>
+      <div className="mt-1">
+        <button
+          className="text-black text-base font-bold hover:text-gray-800"
+          onClick={toggleSubMenu}
+        >
+          {isOpen ? 'Cerrar' : 'Ver más'}
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default NavBar;
+
 
